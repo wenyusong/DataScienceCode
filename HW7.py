f89834c29e674ad9f5c09b92c17b3a56845d10b3
Wenyu <wenyu@Ligon-NG2-4.local>
2017-01-18 17:51:40 -0500

    the second modi"
    @@#
    
    q!

--- HW7.py
+++ HW7.py
@@ -16,4 +16,28 @@ import pydot
 
 #!pip install pydot
 
-
+def plot_tree_boundary(x, y, model, title, ax):
+    ax.scatter(x[y==1,0], x[y==1,1], c='green')
+    ax.scatter(x[y==0,0], x[y==0,1], c='white')
+    
+    interval=np.arange(0,1,0.01)
+    n=np.size(interval)
+    x1, x2=np.meshgrid(interval, interval)
+    x1=x1.reshape(-1,1)
+    x2=x2.reshape(-1,1)
+    xx=np.concatenate((x1, x2), axis=1)
+    
+    yy=model.predict(xx)
+    yy=yy.reshape((n,n))
+    
+    x1=x1.reshape(n,n)
+    x2=x2.reshape(n,n)
+    ax.contourf(x1,x2, yy, alpha=0.1, cmap='Greens')
+    
+    ax.set_title(title)
+    ax.set_xlabel('Latitute')
+    ax.set_ylabel('Longitude')
+    
+    return ax
+    
+    
