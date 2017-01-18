cc5986dc281e323a9bf07b93b46bc957facdf545
Wenyu <wenyu@Ligon-NG2-4.local>
2017-01-18 17:28:48 -0500

    added hw7

---
+++ HW7.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn import discriminant_analysis
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn import ensemble
+from sklearn.cross_validation import KFold
+import StringIO
+import matplotlib
+import pydot
+
+#!pip install pydot
+
+
